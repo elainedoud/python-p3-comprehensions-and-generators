b04dd59ce391915898b38433ca0f9b89515c782a
Elaine <elainedoud@gmail.com>
2024-02-13 20:35:07 -0500

    Lab now completed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    return_evens = [(n%2 == 0) for n in range(num_list)]
-#Goal is to return all even elements in a list of integers
-#So we want to do x%2 == 0, with a for loop to iterate over the numbers
+    return [n for n in num_list if n%2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    return [(n + "!") for n in sentence_list]
